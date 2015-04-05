9f475a7e2d56de7e66387512a17b050ba9a8fa39
FG Ribreau <github@fgribreau.com>
2015-04-05 20:48:21 +0200

    fix(lint)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
   Overly tolerant url parser
  */
@@ -6,64 +7,67 @@ var Url = module.exports = {
   PROTOCOL: '://',
   COLON: ':',
   AT: '@',
-  parse: function(url){
+  parse: function (url) {
     var parsed = {
       protocol: null, // string
       auth: null, // string
       hostname: null, // string
       host: null, // string
       port: null, // string
-      href: url // string
+      href: url, // string
+      path: null
     };
 
     // check protocol
     var protocolIdx = url.indexOf(this.PROTOCOL);
-    if(protocolIdx === -1){
-      // protocol is not present add one
-    } else {
-      parsed.protocol = url.substring(0, protocolIdx+1);
+    if (protocolIdx !== -1) {
+      parsed.protocol = url.substring(0, protocolIdx + 1);
       protocolIdx += this.PROTOCOL.length;
       // remove the protocol part from the url
       url = url.substring(protocolIdx);
     }
 
-    var atCount        = strCount(url, this.AT);
-    var colonCount     = strCount(url, this.COLON);
-    var atIsPresent    = atCount > 0;
+    var atCount = strCount(url, this.AT);
+    var colonCount = strCount(url, this.COLON);
+    var atIsPresent = atCount > 0;
     var colonIsPresent = colonCount > 0;
 
-    if(!atIsPresent && !colonIsPresent){
-      parsed.host     = url;
+    if (!atIsPresent && !colonIsPresent) {
+      parsed.host = url;
       parsed.hostname = url;
       // parse is done, there are no port and auth
       return parsed;
     }
 
-    if(colonCount === 1 && !atIsPresent){ // it can only be the port number
+    if (colonCount === 1 && !atIsPresent) { // it can only be the port number
       extractPortAndHostnameFromHost(parsed, url);
       return parsed;
     }
 
-    var parts   = url.split(this.AT);
-    var host    = parts.pop();
+    var parts = url.split(this.AT);
+    var host = parts.pop();
     extractPortAndHostnameFromHost(parsed, host);
     parsed.auth = parts.join(this.AT);
     return parsed;
   }
 };
 
-function strCount(str, substr){
-  if (str === null || substr === null) return 0;
-  str    = String(str);
+function strCount(str, substr) {
+  if (str === null || substr === null) {
+    return 0;
+  }
+  str = String(str);
   substr = String(substr);
 
-  var count  = 0,
-      pos    = 0,
-      length = substr.length;
+  var count = 0,
+    pos = 0,
+    length = substr.length;
 
   while (true) {
     pos = str.indexOf(substr, pos);
-    if (pos === -1) break;
+    if (pos === -1) {
+      break;
+    }
     count++;
     pos += length;
   }
@@ -71,11 +75,11 @@ function strCount(str, substr){
   return count;
 }
 
-function extractPortAndHostnameFromHost(parsed, host){
+function extractPortAndHostnameFromHost(parsed, host) {
   var portIdx = host.indexOf(Url.COLON);
   parsed.host = host;
 
-  if(portIdx === -1){
+  if (portIdx === -1) {
     parsed.hostname = host;
     // no colon is present inside host
     return;
